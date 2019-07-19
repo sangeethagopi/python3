456b83b64573727f8076b9bdc838b0b3197a7b5f
sangeethagopi <50619394+sangeethagopi@users.noreply.github.com>
2019-07-19 09:30:08 +0530

    Update bdd.py

--- bdd.py
+++ bdd.py
@@ -1,9 +1,9 @@
-a,b=map(int,input().split())
-for num in range(a,b):
+ac,bc=map(int,input().split())
+for num1 in range(ac,bc):
   sum=0
-  temp=num
+  temp=num1
   while(temp>0):
      sum=sum+(temp%10)**3
      temp=temp//10
-  if(sum==num):
-     print(num,end=" ")
+  if(sum==num1):
+     print(num1,end=" ")
