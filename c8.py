8c8f570b3e115f4c45862c9a27e16b2c7507917e
sangeethagopi <50619394+sangeethagopi@users.noreply.github.com>
2019-07-19 10:32:23 +0530

    Create c8.py

---
+++ c8.py
@@ -0,0 +1,17 @@
+gc=int(input())
+ck=[]
+for i in range (0,c):
+	kc.append(input())
+l1=len(k[0])
+e1=""
+for i in range (0,l1):
+	c=k[0][i]
+	f=0
+	for j in range (0,gc):
+		if(c!=kc[j][i]):
+			f=1
+	if(f==0):
+		e1=e1+c
+	else:
+		break
+print(e1)
