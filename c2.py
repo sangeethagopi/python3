6c3d67e3a5068eb256f09376cbf4b48901bc5dff
sangeethagopi <50619394+sangeethagopi@users.noreply.github.com>
2019-07-19 10:05:20 +0530

    Create c2.py

---
+++ c2.py
@@ -0,0 +1,8 @@
+mac=input()
+sic=0
+for word in mac:
+  
+  if word=='.'or word=='@'or word=='%'or word=='&'or word=='*'or word=='6'or word=='#'or word=='!'or word=='$' or word=='_':
+    sci+=1
+  
+print(sic) 
