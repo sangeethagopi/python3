1e021739cf4ed380e65d1efb6a410ecc76dd1329
sangeethagopi <50619394+sangeethagopi@users.noreply.github.com>
2019-07-19 12:03:51 +0530

    Update az27.py

--- az27.py
+++ az27.py
@@ -1,6 +1,6 @@
-sc1,sc2,sc3=map(int,input().split())
+zc1,uc2,bc3=map(int,input().split())
 num4=0
-for i in range(0,s3):
-    num4=num4+sc1
-    sc1=sc1+sc2
+for i in range(0,bc3):
+    num4=num4+z1
+    zc1=zc1+uc2
 print(num4)
